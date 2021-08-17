679941d0dfae277d3cf80e40821003988500d12f
Natanaelel <43886350+Natanaelel@users.noreply.github.com>
2021-08-17 22:12:31 +0200

    Update text.js

--- text.js
+++ text.js
@@ -1,1 +1,46 @@
+function setup(g){
+    my_spirits.forEach(addSpirit)
+    
+    
+    
+}
+if(tick == 0)setup(this)
 
+
+function addSpirit(spirit){
+    memory[spirit.id] = {
+        task: "farm",
+        subtask: "find star",
+        subsubtask: ""
+    }
+}
+
+
+function main(){
+    my_spirits.forEach(spirit=>{
+        if(!memory[spirit.id])addSpirit(spirit)
+    })
+    my_spirits.forEach(doTask)
+    
+}
+function doTask(spirit){
+    switch (memory[spirit.id].task) {
+        case "farm":
+            
+            break;
+        
+        default:
+            // code
+    }
+    for(s of my_spirits){
+        s.move(enemy_base.position)
+	    s.energize(enemy_base)
+    }    
+    
+    
+}
+
+
+
+
+main()
